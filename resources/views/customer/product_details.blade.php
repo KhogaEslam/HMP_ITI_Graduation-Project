68200d5b6c5fdfdb89a725bc1d235591b1ecded2
MernaShakerAttia <merna_shaker@yahoo.com>
2017-05-28 09:26:20 +0200

    some change

--- resources/views/customer/product_details.blade.php
+++ resources/views/customer/product_details.blade.php
@@ -1,4 +1,10 @@
+@extends('layouts.customer')
+@section('title')
+    Products
+@endsection
+@section('content')
 <p>Product name: {{$product->name}}</p>
 <p>Product price: {{$product->price}}</p>
 <p>Product quantity: {{$product->quantity}}</p>
 <p>Category: {{$category->name}}</p>
+@endsection
