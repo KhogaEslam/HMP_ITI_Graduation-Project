131dc2d2c2a3a8b64dc5194f129b3ba019f7de5a
MernaShakerAttia <merna_shaker@yahoo.com>
2017-05-31 16:44:38 +0200

    temp commit

--- resources/views/customer/cart.blade.php
+++ resources/views/customer/cart.blade.php
@@ -57,27 +57,24 @@
             </div>
             {{-- check out --}}
             <div class="col-md-4 text-center">
-                {{--@foreach($cartDetails as $cartDetail)--}}
-                    {{--<input type="hidden" name="item_name" value="{{$cartDetail->product->name}}">--}}
-                    {{--<input type="hidden" name="item_number" value="{{$cartDetail->quantity}}">--}}
-                    {{--<input type="hidden" name="amount" value="{{$cartDetail->product->price}}">--}}
-                {{--@endforeach--}}
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
                     <input type="hidden" name="cmd" value="_xclick">
-                    <input type="hidden" name="upload" value="0">
-                    <input type="hidden" name="no_note" value="0">
-                    <input type="hidden" name="business" value="Gadgetly">
+                    <input type="hidden" name="business" value="mgmhardwaremarketplace@gmail.com">
 
-                    <input type="hidden" name="item_name" value="Donation">
-                    <input type="hidden" name="item_number" value="1">
-                    <input type="hidden" name="amount" value="9.00">
-                    <input type="hidden" name="no_shipping" value="0">
+                    <input type="hidden" name="item_name_1" value="Donation">
+                    <input type="hidden" name="item_number_1" value="1">
+                    <input type="hidden" name="amount_1" value="9.00">
+                    <input type="hidden" name="no_shipping_1" value="0">
 
+                    <input type="hidden" name="item_name_2" value="Donation">
+                    <input type="hidden" name="item_number_2" value="1">
+                    <input type="hidden" name="amount_1" value="9.00">
+                    <input type="hidden" name="no_shipping_1" value="0">
+
+                    <input type="hidden" name="no_note" value="1">
                     <input type="hidden" name="currency_code" value="USD">
                     <input type="hidden" name="lc" value="AU">
                     <input type="hidden" name="bn" value="PP-BuyNowBF">
-
-                    <input type="hidden" name="return" value="http://hmp.local/">
                     <input type="image" src="https://www.paypal.com/en_AU/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
                     <img alt="" border="0" src="https://www.paypal.com/en_AU/i/scr/pixel.gif" width="1" height="1">
                 </form>
